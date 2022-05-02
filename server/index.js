187b5ec34f7161b6055ae4ef04e375216c792183
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-05-01 22:11:25 -0300

    #11-installing web-push on node

--- server/index.js
+++ server/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const bodyParse = require('body-parser');
 const { insuranceSave, insuranceList } = require('./insurance-service');
+const webpush = require('web-push');
+
+// VAPID keys should be generated only once.
+const vapidKeys = {
+  publicKey: 'BEVRxq6wJ7nFBfkdYS8lyMojMr8fkg7mG4EHq71x_G35a5grc8Iu31lsKRC6uCQ82qI1V0EOJjskaj5VYDRUwZ8',
+  privateKey: 'DMAU9Ym8AsWvyCt1ZypvppLrr43kuKpxZouNcuV5Vu8'
+}
+
+webpush.setVapidDetails(
+  'test@gmail.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+)
 
 const app = express();
 
